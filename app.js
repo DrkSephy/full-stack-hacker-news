de65f16d24516a90109067c77819002811af9f54
David Leonard <sephirothcloud1025@yahoo.com>
2015-05-23 10:51:01 -0400

    Defining new route for individual posts, defining Post Controller

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ function($stateProvider, $urlRouterProvider){
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/post/{id}',
+      templateUrl: '/posts.html',
+      controller: 'Posts.Ctrl'
     });
 
   $urlRouterProvider.otherwise('home');
@@ -25,7 +31,11 @@ function($scope, posts){
   	$scope.posts.push({
   		title: $scope.title, 
   		link: $scope.link,
-  		upvotes: 0
+  		upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
   	});
   	$scope.title = '';
   	$scope.link = '';
@@ -36,13 +46,17 @@ function($scope, posts){
   }
 }]);
 
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+
+}]);
+
 app.factory('posts', [function(){
   var o = {
-  	posts: [{title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}]
+  	posts: []
   };
   return o;
 }]);
