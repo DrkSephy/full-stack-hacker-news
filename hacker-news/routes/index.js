b9260cc7cce546325a64da8b40772b6b3a236405
David Leonard <sephirothcloud1025@yahoo.com>
2015-05-26 10:28:56 -0400

    Adding route for getting posts

--- hacker-news/routes/index.js
+++ hacker-news/routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+
+/* GET route for retrieving posts */
+router.get('/posts', function(req, res, next){
+	Post.find(function(err, posts){
+		if(err){ return next(err); }
+
+		res.json(posts);
+	});
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
