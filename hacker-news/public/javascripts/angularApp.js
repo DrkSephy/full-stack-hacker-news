b5460268376b18479168e7f8c3a67ac2e7128920
David Leonard <sephirothcloud1025@yahoo.com>
2015-05-28 17:11:39 -0400

    Loading posts using resolve property of UI Router

--- hacker-news/public/javascripts/angularApp.js
+++ hacker-news/public/javascripts/angularApp.js
@@ -9,7 +9,12 @@ function($stateProvider, $urlRouterProvider) {
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
-            controller: 'MainCtrl'
+            controller: 'MainCtrl',
+            resolve: {
+                postPromise: ['posts', function(posts){
+                    return posts.getAll();
+                }]
+            }
         })
 
         .state('posts', {
